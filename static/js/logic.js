4e0d7355c9d831259d37250b6563745f2db90b29
thn22 <107218288+thn22@users.noreply.github.com>
2022-10-16 18:57:12 -0500

    adding js charts

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,27 +1,45 @@
 
-d3.select("#submit").on("click", function(targ) {
-  d3.event.preventDefault();
-  let player = d3.select("#name").property("value");
-  if(!player.length) {
-    return;
-  }
-  d3.json(`http://127.0.0.1:5000/api/v1.0/searchbyname/${player}`).then(function(data) {
+  d3.json(`http://127.0.0.1:5000/api/v1.0/all`).then(function(data) {
     console.log(data);
-    let names = data.map(x => x.name);
-    let table = d3.select("#table tbody");
-    table.html("");
-    for (x of data) {
-      let row = table.append("tr");
-      row.append("td").text(x.name);
-      row.append("td").text(x.age);
-      row.append("td").text(x.team);
-      row.append("td").text(x.position);
-      row.append("td").text(x.targets);
-      row.append("td").text(x.receptions);
-      row.append("td").text(x.catchpct);
-      row.append("td").text(x.yards);
-      row.append("td").text(x.touchdowns);
+    
+    let yards_catch = {
+      yards: data.map(player=>player.Yards),
+      catch: data.map(player=>player.Receptions)
     }
-  });
+    let age_catch = {
+      age: data.map(player=>player.Age),
+      pct: data.map(player=>parseFloat(player["Catch Percentage"].replace("%","")))
+    }
+    let pos_rec = {
+      position: data.map(player=>player.Position),
+      catch: data.map(player=>player.Receptions)
+    }
+
+    var trace1 = {
+      x: yards_catch.yards,
+      y: yards_catch.catch,
+      mode: 'markers',
+      type: 'scatter'
+    };
+
+    var trace2 = {
+      x: age_catch.age,
+      y: age_catch.pct,
+      mode: 'markers',
+      type: 'scatter'
+    };
+
+    var trace3 = {
+      x: pos_rec.position,
+      y: pos_rec.catch,
+      mode: 'markers',
+      type: 'scatter'
+    };
+
+    Plotly.newPlot('chart_1', [trace1]);
+    Plotly.newPlot('chart_2', [trace2]);
+    Plotly.newPlot('chart_3', [trace3]);
+
+
 
 })
